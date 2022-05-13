a8d34abfc07ff825fcde835d707d9950f565b803
Mauriciosz97 <104545311+Mauriciosz97@users.noreply.github.com>
2022-05-12 22:21:35 -0300

    exercicios de funcoes

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,29 @@
+// function minhaFuncao(variavel) {
+// 	return variavel * 5
+// }
+
+// console.log(minhaFuncao(2))
+// console.log(minhaFuncao(10))
+
+//10 50
+
+//retirando o console.log não aparece nada no console
+
+// let textoDoUsuario = prompt("Insira um texto");
+
+// const outraFuncao = function(texto) {
+// 	return texto.toLowerCase().includes("cenoura")
+// }
+
+// const resposta = outraFuncao(textoDoUsuario)
+// console.log(resposta)
+
+//esta função te manda digitar um texto e te retorna true se etiver cenoura escrita no texto.
+
+//I. True
+//II. True
+//III. True
+ 	
+// "Eu sou Caio, tenho 23 anos, moro em São Paulo e sou estudante."
+ 
+// function
