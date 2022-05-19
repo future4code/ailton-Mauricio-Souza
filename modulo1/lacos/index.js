aacfd8fdf486433efb9d28316a37c80374122081
Mauriciosz97 <104545311+Mauriciosz97@users.noreply.github.com>
2022-05-19 15:28:26 -0300

    alteracoes-lacos

--- modulo1/lacos/index.js
+++ modulo1/lacos/index.js
@@ -62,26 +62,29 @@
 //resultado 10// 
 
 
-// const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
-// for (let numero of lista) {
-//   if (numero > 18) {
-// 		console.log(numero)
-// 	}
-// }
+// 4
 
 //[19, 21, 23, 25, 27, 30]
 
 //
 
-const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
-let quantidadeAtual = 0
-while(quantidadeAtual < quantidadeTotal){
-  let linha = ""
-  for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
-    linha += "*"
-  }
-  console.log(linha)
-  quantidadeAtual++
-}
+// const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+// let quantidadeAtual = 0
+// while(quantidadeAtual < quantidadeTotal){
+//   let linha = ""
+//   for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+//     linha += "*"
+//   }
+//   console.log(linha)
+//   quantidadeAtual++
+// }
+
+// **** e erro
+
+
+//exercicios 1
+
+
+
 
 
