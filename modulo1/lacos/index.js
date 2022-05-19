0199fb5195b976c610d549dc5a81a89972522e72
Mauriciosz97 <104545311+Mauriciosz97@users.noreply.github.com>
2022-05-18 22:25:56 -0300

    exercicios-lacos-imcompleto

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,87 @@
+// let numeroUsuario = +prompt('digite um numero:')
+// const numerosDigitados = []
+// numerosDigitados.push(numeroUsuario)
+
+// while (numeroUsuario !== 0){
+//     numeroUsuario = +prompt('digite um numero:')
+
+//     numerosDigitados.push(numeroUsuario)
+// }
+// console.log(numerosDigitados)
+
+// let contador = 0
+// let soma = 0
+
+// while (contador < numerosDigitados.length){
+//     soma = soma + numerosDigitados [contador]
+
+//     contador++
+// }
+// console.log(soma)
+
+// const maior = (arr) => {
+//     let maior = 0
+//     for(let i = 0; i < arr.length; i++) {
+//    const elemento = arr[i]
+//    if (elemento > maior) {
+
+//    }
+//  }
+// console.log(maior)
+// }
+// maior([1,2,3])
+
+// let numeros = [1,2,3,4,5]
+
+// for(let numero of numeros) {
+//     console.log(numero)
+// }
+
+// const juntaPalavras = (arr) => {
+//     let frase = ""
+
+//     for(let palavra of arr) {
+// frase = frase + " "
+// frase = frase + palavra
+//     }
+//     console.log(frase)
+// }
+
+// juntaPalavras(["oi", "tudo na paz?"])
+
+
+
+//interpretação 1
+
+// let valor = 0
+// for(let i = 0; i < 5; i++) {
+//   valor += i
+// }
+// console.log(valor)
+
+//resultado 10// 
+
+
+// const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+// for (let numero of lista) {
+//   if (numero > 18) {
+// 		console.log(numero)
+// 	}
+// }
+
+//[19, 21, 23, 25, 27, 30]
+
+//
+
+const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+let quantidadeAtual = 0
+while(quantidadeAtual < quantidadeTotal){
+  let linha = ""
+  for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+    linha += "*"
+  }
+  console.log(linha)
+  quantidadeAtual++
+}
+
+
