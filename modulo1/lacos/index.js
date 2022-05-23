d07a0d830d90797d0a8a1faf2bb8cdc8f1731164
Mauriciosz97 <104545311+Mauriciosz97@users.noreply.github.com>
2022-05-22 23:52:02 -0300

    laços-revisado

--- modulo1/lacos/index.js
+++ modulo1/lacos/index.js
@@ -82,8 +82,117 @@
 // **** e erro
 
 
-//exercicios 1
+//exercicios 1 interpretaçao
 
+// let valor = 0
+// for(let i = 0; i < 5; i++) {
+//   valor += i
+// }
+// console.log(valor)
+
+//10
+
+
+// const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+// for (let numero of lista) {
+//   if (numero > 18) {
+// 		console.log(numero)
+// 	}
+
+// }
+
+//todos os numeros maiores de 18 (19,21,23,25,27,30)
+
+
+
+// const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+// let quantidadeAtual = 0
+// while(quantidadeAtual < quantidadeTotal){
+//   let linha = ""
+//   for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+//     linha += "*"
+//   }
+//   console.log(linha)
+//   quantidadeAtual++
+
+
+// **** e um erro
+
+
+//exercicios de escrita
+
+1
+
+// const bichosEstimacao = +prompt('quantos bichinhos voce tem ?');
+// console.log(bichosEstimacao)
+// if (bichosEstimacao === 0){
+//     console.log('triste! adote logo seu pet ...')
+// }
+
+// if(bichosEstimacao > 0){
+//    let bichos = []
+//    for(let i = 0; i < bichosEstimacao; i++)
+
+// bichos.push(prompt('qual nome do seu pet?'))
+
+// }
+// console.log(bichos)
+
+
+
+
+2
+
+const arrayOriginal = [5, 22, 98, 121, 10]
+
+function valorDoArrayOriginal (arrayOriginal) {
+    for(valor of arrayOriginal){
+        console.log(valor)
+    }
+}
+valorDoArrayOriginal (arrayOriginal)
+
+function valorDoArrayOriginal (arrayOriginal){
+    for(valor of arrayOriginal){
+        console.log (valor/10)
+    }
+}
+
+function valoresPares (arrayOriginal) {
+    arrayNumerosPares = []
+        for (valor of arrayOriginal){
+            if(valor % 2 === 0){
+                arrayNumerosPares.push(valor)
+            }
+        }
+    console.log(arrayNumerosPares)
+    }
+valoresPares(arrayOriginal)
+
+
+function arrayDeString (arrayOriginal)
+let arrayDeString2 = []
+let i = 0
+for(valor of arrayOriginal){
+    arrayDeString2.push (`o elemento do index ${i} é: ${valor}`)
+    i++
+}
+console.log(arrayDeString2)
+
+function maiorOuMenorNumero (arrayOriginal){
+    let maiorNumero = 0;
+    menorNumero = Infinity;
+    for(let i = 0; i , , arrayOriginal.length; i++){
+
+        if(arrayOriginal[i] < menorNumero){
+            menorNumero = arrayOriginal[i]
+        }
+    }else (arrayOriginal[i] > maiorNumero){
+            maiorNumero = arrayOriginal[i]
+        }
+    }
+    console.log('O maior numero é:', maiorNumero)
+    console.log("o menor numero é:", menorNumero)
 
 
 
